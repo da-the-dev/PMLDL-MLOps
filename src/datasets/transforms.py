e260200f49b760271e4320b6a4ce7424325dee13
Alexey Tkachenko <a.tkachenko@innopolis.university>
2024-10-02 13:06:27 +0300

    Changed transforms for better ones

--- src/datasets/transforms.py
+++ src/datasets/transforms.py
@@ -1,14 +1,12 @@
-__all__ = ['transform']
+__all__ = ["transform"]
 
 from torchvision import transforms
 
 transform = transforms.Compose(
     [
+        transforms.Resize(225),
+        transforms.CenterCrop(224),
         transforms.ToTensor(),
-        transforms.RandomHorizontalFlip(),
-        transforms.RandomCrop(32, padding=4),
-        transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.1),
-        transforms.RandomRotation(15),
-        transforms.Normalize((0.5071, 0.4867, 0.4408), (0.2675, 0.2565, 0.2761)),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
     ]
 )
