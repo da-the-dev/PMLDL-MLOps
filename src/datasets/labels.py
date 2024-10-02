a68568d6e6069070345b57f2211d8eedd15a13e9
Alexey Tkachenko <a.tkachenko@innopolis.university>
2024-10-02 17:43:07 +0300

    Added labels as a module

---
+++ src/datasets/labels.py
@@ -0,0 +1,24 @@
+__all__ = ["course_labels"]
+
+course_classes = [
+    "aquatic mammals",
+    "fish",
+    "flowers",
+    "food containers",
+    "fruit and vegetables",
+    "household electrical devices",
+    "household furniture",
+    "insects",
+    "large carnivores",
+    "large man-made outdoor things",
+    "large natural outdoor scenes",
+    "large omnivores and herbivores",
+    "medium-sized mammals",
+    "non-insect invertebrates",
+    "people",
+    "reptiles",
+    "small mammals",
+    "trees",
+    "vehicles 1",
+    "vehicles 2",
+]
